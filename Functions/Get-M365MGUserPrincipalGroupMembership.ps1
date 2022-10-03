8584b2dbeca013135d62fa2f084605b0942efe17
djust270 <dave.just@gmail.com>
2022-10-03 08:13:17 -0400

    updated filename

---
+++ Functions/Get-M365MGUserPrincipalGroupMembership.ps1
@@ -0,0 +1,10 @@
+function Get-M365MGUserPrincipalGroupMembership{
+param (
+		[string]$UserID
+	)
+	$graphApiVersion = "v1.0"
+	$Resource = "users/$UserID/memberOf/$/microsoft.graph.group?$select=id,displayName,securityEnabled,groupTypes,onPremisesSyncEnabled"
+	$uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+	$groups = (Invoke-MgGraphRequest -Uri $uri -Method Get).Value
+	return $groups
+}
