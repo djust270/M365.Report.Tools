6425d74338c9028c835ff420843a9aea54c6e338
djust270 <dave.just@gmail.com>
2022-10-03 06:59:53 -0400

    Updated function

--- Functions/Get-M365TeamsChannelReport.ps1
+++ Functions/Get-M365TeamsChannelReport.ps1
@@ -1,17 +1,26 @@
 function Get-M365TeamsChannelReport 
 {
+    $RequiredScopes = @(
+    'Channel.ReadBasic.All'
+	'Team.ReadBasic.All'
+	'TeamMember.Read.All')
+    Set-M365MGGraphConnectionScopes -RequiredScopes $RequiredScopes
     $TeamsChannelReport = [System.Collections.Generic.List[PsObject]]::new()
     $Teams = Get-MgTeam -All
-    foreach ($Team in $Teams){    
+    $i = 1
+    foreach ($Team in $Teams){
+        Write-Progress -Activity "Processing Team Channel Report" -Status "Working on Team $($Team.DisplayName)" -PercentComplete (($i / $Teams.Count) * 100)    
         $Channels = Get-MgTeamChannel -TeamId $Team.id
         $Channels | ForEach-Object {
 			$TeamsChannelReport.Add(
 				[pscustomobject]@{
 					'Team' = $Team.displayname
 					'Channel' = $_.displayname
+                    'ChannelMembers' = ((Get-MgTeamChannelMember -TeamId $Team.id -ChannelId $_.id).additionalproperties.email) -join ' , '
 				}
 			)
 		}
+        $i++
     }
     return $TeamsChannelReport
 }
